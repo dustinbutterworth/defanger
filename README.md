2fb23d4728aace75f93452e3ec2a27fe3ca6c853
dustinbutterworth <51181391+dustinbutterworth@users.noreply.github.com>
2023-03-20 23:12:49 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Defanger.py
+
+To defang a url:
+```
+python defang.py --url https://www.google.com
+python defang.py --url 8.8.8.8
+```
+
+To defang a file:
+```
+python defang.py --file ./test/test.txt
+```
---
+++ defang.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+import os
+import argparse
+from pathlib import Path
+
+
+def defang_url(url: str) -> str:
+    url = url.split(".")
+    separator = "[.]"
+    url = separator.join(url)
+    url = url.replace("http", "hxxp")
+    url = url.replace("@", "[AT]")
+    return url
+
+
+def defang_file(filename: Path):
+    infile = open(filename, 'r')
+    outfile_path = os.path.dirname(os.path.abspath(filename))
+    outfile = open(
+        f'{outfile_path}/{filename.stem}_defanged{filename.suffix}', "w")
+    for line in infile:
+        str = defang_url(line)
+        outfile.write(str)
+    outfile.close()
+    infile.close()
+    print(f'Created new defanged version of {infile.name} at {outfile.name}')
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file',
+                        type=Path,
+                        help="Provide the path to a file to defang."
+                        )
+    parser.add_argument('--url',
+                        type=str,
+                        help="Provide a url to defang."
+                        )
+    args = parser.parse_args()
+
+    if args.url:
+        print(defang_url(args.url))
+    elif args.file:
+        defang_file(args.file)
+    else:
+        parser.error('Please provide --url or --file. Run defang.py --help'
+                     ' for more information')
+
+
+if __name__ == "__main__":
+    main()
